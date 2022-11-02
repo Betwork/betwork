52d1f31768a05e0c2741bc37466edecf8badb1af
mattgolden100 <72214886+mattgolden100@users.noreply.github.com>
2022-11-02 13:23:24 -0400

    Fixed friends.feature and added bets.feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_01_024446) do
+ActiveRecord::Schema.define(version: 2017_06_13_183249) do
 
   create_table "activities", force: :cascade do |t|
     t.string "trackable_type"
@@ -21,11 +21,14 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.text "parameters"
     t.string "recipient_type"
     t.integer "recipient_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+    t.index ["owner_type", "owner_id"], name: "index_activities_on_owner_type_and_owner_id"
     t.index ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+    t.index ["recipient_type", "recipient_id"], name: "index_activities_on_recipient_type_and_recipient_id"
     t.index ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+    t.index ["trackable_type", "trackable_id"], name: "index_activities_on_trackable_type_and_trackable_id"
   end
 
   create_table "badges_sashes", force: :cascade do |t|
@@ -56,10 +59,11 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.integer "commentable_id"
     t.integer "user_id"
     t.string "role", default: "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "comment_html"
     t.index ["commentable_id"], name: "index_comments_on_commentable_id"
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
     t.index ["commentable_type"], name: "index_comments_on_commentable_type"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -68,8 +72,8 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.string "name"
     t.datetime "event_datetime"
     t.integer "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "cached_votes_up", default: 0
     t.integer "comments_count", default: 0
     t.index ["cached_votes_up"], name: "index_events_on_cached_votes_up"
@@ -83,10 +87,12 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.string "follower_type", null: false
     t.integer "follower_id", null: false
     t.boolean "blocked", default: false, null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["followable_id", "followable_type"], name: "fk_followables"
+    t.index ["followable_type", "followable_id"], name: "index_follows_on_followable_type_and_followable_id"
     t.index ["follower_id", "follower_type"], name: "fk_follows"
+    t.index ["follower_type", "follower_id"], name: "index_follows_on_follower_type_and_follower_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -110,8 +116,8 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.integer "target_id"
     t.text "target_data"
     t.boolean "processed", default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "merit_activity_logs", force: :cascade do |t|
@@ -127,11 +133,13 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.integer "num_points", default: 0
     t.string "log"
     t.datetime "created_at"
+    t.index ["score_id"], name: "index_merit_score_points_on_score_id"
   end
 
   create_table "merit_scores", force: :cascade do |t|
     t.integer "sash_id"
     t.string "category", default: "default"
+    t.index ["sash_id"], name: "index_merit_scores_on_sash_id"
   end
 
   create_table "odds", force: :cascade do |t|
@@ -144,8 +152,8 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.text "content", null: false
     t.integer "user_id"
     t.string "attachment"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "cached_votes_up", default: 0
     t.integer "comments_count", default: 0
     t.text "content_html"
@@ -155,8 +163,8 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
   end
 
   create_table "sashes", force: :cascade do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -201,10 +209,12 @@ ActiveRecord::Schema.define(version: 2022_10_01_024446) do
     t.boolean "vote_flag"
     t.string "vote_scope"
     t.integer "vote_weight"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["votable_id", "votable_type", "vote_scope"], name: "index_votes_on_votable_id_and_votable_type_and_vote_scope"
+    t.index ["votable_type", "votable_id"], name: "index_votes_on_votable_type_and_votable_id"
     t.index ["voter_id", "voter_type", "vote_scope"], name: "index_votes_on_voter_id_and_voter_type_and_vote_scope"
+    t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
 end
