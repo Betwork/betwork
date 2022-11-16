9ce6e68973f92fad12bd4b91837def0eb0625ec8
Solomon Chang <sjc2233@columbia.edu>
2022-11-15 22:58:10 -0500

    post controller destroy finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,14 +3,14 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def show
-    puts "TRIEGGER show"
+    # puts "TRIEGGER show"
     @comments = @post.comments.all
   end
 
   def create
-    puts "TRIEGGER create"
-    puts params
-    puts post_params
+    # puts "TRIEGGER create"
+    # puts params
+    # puts post_params
     # puts "TEST THIS OUT NOW PLEASE"
     @post = current_user.posts.new(post_params)
     if @post.save
@@ -21,12 +21,12 @@ class PostsController < ApplicationController
   end
 
   def edit
-    puts "TRIEGGER edit"
+    # puts "TRIEGGER edit"
     # puts post_params
   end
 
   def update
-    puts "TRIEGGER update"
+    # puts "TRIEGGER update"
     # puts post_params
     # puts "before update content"
     # puts @post.content
@@ -37,8 +37,10 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    puts "TRIEGGER destroy"
+    # puts "TRIEGGER destroy"
+    # puts Post.all.length()
     @post.destroy
+    # puts Post.all.length()
     respond_to do |format|
       format.js
       format.html { redirect_to root_path }
@@ -48,14 +50,14 @@ class PostsController < ApplicationController
   private
 
   def set_post
-    puts "TRIEGGER set_post"
+    # puts "TRIEGGER set_post"
     @post = Post.find_by(id: params[:id])
     # puts @post
     render_404 and return unless @post && User.find_by(id: @post.user_id)
   end
 
   def post_params
-    puts "TRIEGGER post_params"
+    # puts "TRIEGGER post_params"
     params.require(:post).permit(:content, :attachment)
   end
 end
--- spec/controllers/post_controller_spec.rb
+++ spec/controllers/post_controller_spec.rb
@@ -30,17 +30,19 @@ RSpec.describe "Posts", type: :request do
     expect(allPosts[0].content).to eq('CHANGED POST CONTENT')
   end
 
-  it "should be able to create a post" do
+  it "should be able to delete a post" do
     @user = User.create!(name: 'Betty', email: 'Betty@betwork.com', password: 'password')
     @user.skip_confirmation!
     @user.save!
     sign_in @user
-    # @params = { :id => @user, :post => { :content => 'hi' } }
-    @params = { :id => 100, :post => { :content => 'hi' } }
-    # @params = { :id => 100, :post => { :content => 'hi', :user_id => @user.id, :content_html => "<p>hi/p>" } }
-    post posts_path(@params)
+    # # @params = { :id => @user, :post => { :content => 'hi' } }
+    # @params = { :id => 100, :post => { :content => 'hi' } }
+    # # @params = { :id => 100, :post => { :content => 'hi', :user_id => @user.id, :content_html => "<p>hi/p>" } }
+    # post posts_path(@params)
+    @post = Post.create!("content": "hi", "user_id": @user.id, "created_at": "2022-11-02 21:31:39.931748", "updated_at": "2022-11-02 21:31:39.931748", "content_html": "<p>hi</p>")
     allPosts = Post.all
     expect(allPosts[0].content).to eq('hi')
+    @params = { :id => @post.id }
     delete post_path(@params)
     allPosts = Post.all
     expect(allPosts.length()).to eq(0)
