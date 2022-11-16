fe5c0509a89ddb8ba4c433ff2bda98a5795c70cd
Solomon Chang <sjc2233@columbia.edu>
2022-11-16 13:01:08 -0500

    rspec for odds_controller done

--- app/controllers/odds_controller.rb
+++ app/controllers/odds_controller.rb
@@ -4,91 +4,96 @@ require 'openssl'
 require 'json'
 
 class OddsController < ApplicationController
-    before_action :set_user
+  before_action :set_user
 
-    def index
-        #@odds = Odd.all
-        Odd.delete_all
-        odds = find_nba_odds()
-        odds.each do |odd|
-          new_odd = Odd.create!(
-            "home_team_name": odd['homeTeam'],
-            "away_team_name": odd['awayTeam'],
-            "home_money_line": odd['homeMoneyLine'],
-            "away_money_line": odd['awayMoneyLine'],
-            "date": odd['startTime']
-          )
-          # new_odd = Odd.new
-          # new_odd.home_team_name = odd['homeTeam']
-          # new_odd.away_team_name = odd['awayTeam']
-          # new_odd.home_money_line = odd['homeMoneyLine']
-          # new_odd.away_money_line = odd['awayMoneyLine']
-          # new_odd.date = odd['startTime']
-          new_odd.save
-        end
-        @odds = Odd.all
+  def index
+    # puts "execute index"
+    # puts params
+    @odds = Odd.all
+    Odd.delete_all
+    odds = find_nba_odds()
+    odds.each do |odd|
+      if (!odd['homeTeam'].nil? and
+        !odd['awayTeam'].nil? and
+        !odd['homeMoneyLine'].nil? and
+        !odd['awayMoneyLine'].nil?)
+        new_odd = Odd.create!(
+          "home_team_name": odd['homeTeam'],
+          "away_team_name": odd['awayTeam'],
+          "home_money_line": odd['homeMoneyLine'],
+          "away_money_line": odd['awayMoneyLine'],
+          "date": odd['startTime']
+        )
+        new_odd.save
+      end
     end
+    @odds = Odd.all
+  end
 
-    def friends
-        puts params[:page]
-        @friends = @user.following_users.paginate(page: params[:page])
-        @game = Odd.find params[:id]
-        @amount = params[:amount].nil? ? -1 : params[:amount]
-    end
+  def friends
+    # puts "execute friends"
+    # puts params
+    @friends = @user.following_users.paginate(page: params[:page])
+    @game = Odd.find params[:id]
+    @amount = params[:amount].nil? ? -1 : params[:amount]
+  end
 
-    def show
-        @odds = Odd.all
-    end
+  def show
+    # puts "show"
+    @odds = Odd.all
+  end
 
-private
+  private
 
-def request_api(url)
-  response = Excon.get(
-    url,
-    headers: {
-      'X-RapidAPI-Host' => URI.parse(url).host,
-      #'X-RapidAPI-Key' => "0922e8a07dmsh4cacadace93e259p191ebajsn446e95a439bc"
-      'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
-    }
-  )
+  # def request_api(url)
+  #   puts "request_api"
+  #   response = Excon.get(
+  #     url,
+  #     headers: {
+  #       'X-RapidAPI-Host' => URI.parse(url).host,
+  #       #'X-RapidAPI-Key' => "0922e8a07dmsh4cacadace93e259p191ebajsn446e95a439bc"
+  #       'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+  #     }
+  #   )
+  #
+  #   return nil if response.status != 200
+  #
+  #   puts JSON.parse(response.body)
+  #   JSON.parse(response.body)
+  # end
 
-  return nil if response.status != 200
+  def find_nba_odds()
+    # puts "find_nba_odds"
+    # request_api('https://sports-data3.p.rapidapi.com/nba')
+    url = URI("https://sports-data3.p.rapidapi.com/nba")
 
-  puts JSON.parse(response.body)
-  JSON.parse(response.body)
-end
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-def find_nba_odds()
-  # request_api('https://sports-data3.p.rapidapi.com/nba')
-  url = URI("https://sports-data3.p.rapidapi.com/nba")
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = 'b75f06b51amshedbb7bbb363591fp1d8c49jsnea0e9ea45d3b'
+    request["X-RapidAPI-Host"] = 'sports-data3.p.rapidapi.com'
 
-  http = Net::HTTP.new(url.host, url.port)
-  http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    response = http.request(request)
+    # puts JSON.parse(response.read_body)
+    JSON.parse(response.read_body)
+  end
 
-  request = Net::HTTP::Get.new(url)
-  request["X-RapidAPI-Key"] = 'b75f06b51amshedbb7bbb363591fp1d8c49jsnea0e9ea45d3b'
-  request["X-RapidAPI-Host"] = 'sports-data3.p.rapidapi.com'
+  # def user_params
+  #   puts "user_params"
+  #   params.require(:user).permit(:name, :about, :avatar, :cover,
+  #                                :sex, :dob, :location, :phone_number)
+  # end
+  #
+  # def check_ownership
+  #   puts "check_ownership"
+  #   redirect_to current_user, notice: 'Not Authorized' unless @user == current_user
+  # end
 
-  response = http.request(request)
-  puts JSON.parse(response.read_body)
-  JSON.parse(response.read_body)
-end
-
-
-
-
-def user_params
-  params.require(:user).permit(:name, :about, :avatar, :cover,
-                               :sex, :dob, :location, :phone_number)
-end
-
-def check_ownership
-  redirect_to current_user, notice: 'Not Authorized' unless @user == current_user
-end
-
-def set_user
-  @user = current_user 
-  render_404 and return unless @user
-end
+  def set_user
+    # puts "set_user"
+    @user = current_user
+    render_404 and return unless @user
+  end
 end
--- spec/models/odd_spec.rb
+++ spec/models/odd_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe Odd, type: :model do
 
   it "should be be able to see all odds for all games" do
-    Odd.create([{ home_team_name: 'Los Angeles Lakers', away_team_name: 'Chicago Bulls', home_money_line: +225 }])
-    Odd.create([{ home_team_name: 'Los Angeles Clippers', away_team_name: 'New Orleans Pelicans', home_money_line: -130 }])
-    Odd.create([{ home_team_name: 'Miami Heat', away_team_name: 'Cleveland Cavaliers', home_money_line: -170 }])
-    Odd.create([{ home_team_name: 'Los Angeles Lakers', away_team_name: 'Chicago Bulls', home_money_line: -220 }])
-    Odd.create([{ home_team_name: 'Orlando Magic', away_team_name: 'Golden State Warriors', home_money_line: +310 }])
+    Odd.create([{ home_team_name: 'Los Angeles Lakers',
+                  away_team_name: 'Chicago Bulls',
+                  home_money_line: +225 }])
+    Odd.create([{ home_team_name: 'Los Angeles Clippers',
+                  away_team_name: 'New Orleans Pelicans',
+                  home_money_line: -130 }])
+    Odd.create([{ home_team_name: 'Miami Heat',
+                  away_team_name: 'Cleveland Cavaliers',
+                  home_money_line: -170 }])
+    Odd.create([{ home_team_name: 'Los Angeles Lakers',
+                  away_team_name: 'Chicago Bulls',
+                  home_money_line: -220 }])
+    Odd.create([{ home_team_name: 'Orlando Magic',
+                  away_team_name: 'Golden State Warriors',
+                  home_money_line: +310 }])
     expect(Odd.count == 5)
     @odds = Odd.all
     for odd in @odds
