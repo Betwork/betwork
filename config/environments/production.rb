4a6093b127ef124ca56be3d214c15fb148b3ca0f
Jordi Adoumie <jja2163@columbia.edu>
2022-11-16 11:57:13 -0500

    Darn email won't work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,5 +41,5 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries= true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :letter_opener
 end
